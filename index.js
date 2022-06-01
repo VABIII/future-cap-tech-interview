ba85898891eef162027ab9a6abf9963e48b29e2f
VABIII <tbengtson82@gmail.com>
2022-06-01 16:07:24 -0500

    Code for part 2 written and test cases matching expected output

--- index.js
+++ index.js
@@ -26,31 +26,33 @@ const  DATA = [
     }
 ]
 
+/** Part 1 **/
+
 const TEST_CASE_1 = "FB:12,PLTR:5000"
 const TEST_CASE_2 = "BABA:1,TSLA:5,WISH:1200"
 
 const separateString = str => {
-    let company = str.replace(/[^A-Z]/g, '')
-    let shares = parseInt(str.replace(/\D/g, ''))
+    const company = str.replace(/[^A-Z]/g, '');
+    const shares = parseInt(str.replace(/\D/g, ''));
 
-    return {company, shares}
+    return {company, shares};
 }
 
 
 const calculateValue = portfolio => {
-    let stocks = portfolio.split(',');
-    let newArr = []
+    const stocks = portfolio.split(',');
+    let newArr = [];
     let totalValue = 0;
 
 
     for(let stock of stocks) {
-        newArr.push(separateString(stock))
+        newArr.push(separateString(stock));
     }
 
     for (let i = 0; i < newArr.length; i++) {
-        let currentCompany = newArr[i]
+        let currentCompany = newArr[i];
         for (let j = 0; j < DATA.length; j++) {
-            let currentData = DATA[j]
+            let currentData = DATA[j];
             if(currentCompany.company === currentData.ticker) totalValue += (currentCompany.shares * currentData.close);
         }
     }
@@ -58,12 +60,28 @@ const calculateValue = portfolio => {
     return totalValue;
 }
 
+/** Part 2 **/
 
+const TEST_CASE_3 = "7,1,5,3,6,4"
+const TEST_CASE_4 = "7,6,4,3,1"
 
+const maximizeProfits = prices => {
+    const priceArray = prices.split(',')
+    let maxProfit = 0
 
+    for (let i = 0; i < priceArray.length; i++) {
+        let currentDay = priceArray[i]
+        let profit = 0
+        for (let j = i + 1; j < priceArray.length; j++) {
+            let nextDay = priceArray[j];
+            if(currentDay < nextDay) profit = nextDay - currentDay
+            if(profit > maxProfit) maxProfit = profit
 
+        }
+    }
 
-
+    return maxProfit;
+}
 
 
 
